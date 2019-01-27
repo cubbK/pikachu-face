fe78f211963ac12847ad7c13cedd7ba88d5e3cf8
Dan <danpopa1998@gmail.com>
2019-01-27 18:43:44 +0200

    init expo

---
+++ components/Pikachu.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export class Pikachu extends React.Component {
+  render () {
+    return ()
+  }
+}
---
+++ constants/colors.js
@@ -0,0 +1,8 @@
+export default {
+  background: "#f4dc26",
+  eye: "#232222",
+  iris: "#fff",
+  cheek: "#e92929",
+  nose: "#232222",
+  mouth: "#232222"
+};
